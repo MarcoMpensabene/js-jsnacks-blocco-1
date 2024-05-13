cf3cbe74b952cd94aa01a08c8925e670fba0663d
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-13 12:13:56 +0200

    removed the  space in "Alberto"

--- jsSnack4/script.js
+++ jsSnack4/script.js
@@ -1,5 +1,5 @@
 // * In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
-const nameList = ["Giovanni" , "Ottavio" , "Frabriazio" , "Giuseppina" , "Alberto " , "Riccardo"];
+const nameList = ["Giovanni" , "Ottavio" , "Frabriazio" , "Giuseppina" , "Alberto" , "Riccardo"];
 
 let userName = prompt("Inserisci il tuo nome");
 
