7c9f44c83505466702c68df79673ea511a4de94f
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-13 12:11:13 +0200

    added array , asked a prompt , used a if condition for checking the name

---
+++ jsSnack4/script.js
@@ -0,0 +1,10 @@
+// * In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
+const nameList = ["Giovanni" , "Ottavio" , "Frabriazio" , "Giuseppina" , "Alberto " , "Riccardo"];
+
+let userName = prompt("Inserisci il tuo nome");
+
+if (userName === nameList[0] || userName === nameList[1] || userName === nameList[2] || userName === nameList[3] || userName === nameList[4] || userName === nameList[5] ) {
+    console.log("Puoi partecipare alla festa")
+} else {
+    console.log("vai a casa grazie :D ")
+}
