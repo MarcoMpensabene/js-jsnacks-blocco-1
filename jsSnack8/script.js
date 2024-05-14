a2e3b630f1e076edf939af0754a8ade88a93e03e
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-14 11:45:44 +0200

    added for cicle and if stament with css colors

--- jsSnack8/script.js
+++ jsSnack8/script.js
@@ -11,3 +11,13 @@ divElTwo.id = 'Second';
 
 document.body.appendChild(divEl);
 document.body.appendChild(divElTwo);
+
+const numberList = [ 1 , 2 , 3 ,44 ,77 , 192 , 392 , 888 , 99 ];
+
+for (let i = 0 ; i < numberList.length ; i++){
+    if(numberList[i] % 2 === 1){
+        document.getElementById("First").innerHTML = numberList[i];
+    } else {
+        document.getElementById("Second").innerHTML = numberList[i];
+    }
+}
