632bdec22402fdbb1e58c1e35048b07af775ba7e
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-14 10:54:00 +0200

    didn't remember how to += on somma for adding the value

---
+++ jsSnack6/script.js
@@ -0,0 +1,12 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const intNumber = [ 1 , 2 , 3 , 4 , 5, 6 , 7 , 8, 9 , 10];
+let somma = 0;
+for(let i = 0 ; i < intNumber.length ; i++){
+    console.log(intNumber[i])
+    if (intNumber[i] % 2 === 1){
+        somma += intNumber[i];
+    }
+}console.log(somma)
+
+
