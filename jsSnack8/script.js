59902813566570594fbd0bf7d95202bb6c972ace
MarcoMpensabene <marcompensabene@gmail.com>
2024-05-14 12:01:35 +0200

    added  some ;

--- jsSnack8/script.js
+++ jsSnack8/script.js
@@ -15,12 +15,12 @@ document.body.appendChild(divElTwo);
 const numberList = [ 1 , 2 , 3 ,44 ,77 , 192 , 392 , 888 , 99 ];
 
 for (let i = 0 ; i < numberList.length ; i++){
-        const newEl = document.createElement("p")
-        newEl.append(numberList[i])
+        const newEl = document.createElement("p");
+        newEl.append(numberList[i]);
     
     if(numberList[i] % 2 === 1){
-        divEl.appendChild(newEl)
+        divEl.appendChild(newEl);
     } else {
-        divElTwo.appendChild(newEl)
+        divElTwo.appendChild(newEl);
     }
 }
